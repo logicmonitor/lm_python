9776185ee2563cafd7656a1a686badbe94e876fb
Bennett Borofka <bborofka@me.com>
2016-02-29 15:23:25 -0600

    Update itop_lm_sync.py
    Updated to match flake8 styling.

--- examples/itop_lm_sync.py
+++ examples/itop_lm_sync.py
@@ -22,46 +22,60 @@ from logicmonitor_core.Host import Host
 
 
 def getTree(itop_params, ci_type):
-    url = 'http://' + itop_params["host"] + '/web/webservices/export.php?query=' + itop_params["query_phrasebook"] + '&format=xml'
+    # Concatenate URL
+    url = "".join(["http://",
+                   itop_params["host"],
+                   "/web/webservices/export.php?query=",
+                   itop_params["query_phrasebook"],
+                   "&format=xml"])
 
-    try: 
+    try:
         # Get XML - write to temp file
-        response = requests.get(url, auth=(itop_params["user"], itop_params["password"]))
-        with open('tmp.xml', 'w') as code:
+        response = requests.get(url, auth=(itop_params["user"],
+                                           itop_params["password"]))
+        with open("tmp.xml", "w") as code:
             code.write(response.content)
-        
+
         # Read in XML file into ElementTree object
-        tree = ET.ElementTree(file='tmp.xml')
-        
+        tree = ET.ElementTree(file="tmp.xml")
+
         # Delete temp file
         code.close()
-        os.remove('tmp.xml')
-        
+        os.remove("tmp.xml")
+
         return tree
-    
-    except: sys.exit("ERROR: Can't get XML export from iTop.")
+
+    except:
+        sys.exit("ERROR: Can't get XML export from iTop.")
 
 
 def main():
     # Script arguments
-    parser = argparse.ArgumentParser(description="Synchronize iTop CMDB CIs to LogicMonitor")
-    parser.add_argument("-c", "--company", metavar="CUSTOMER_PORTAL", help="LogicMonitor account", required=True)
-    parser.add_argument("-u", "--user", metavar="USERNAME", help="LogicMonitor username", required=True)
-    parser.add_argument("-p", "--password", metavar="PASSWORD", help="LogicMonitor password", required=True)
-    parser.add_argument("-ih", metavar="ITOP_HOST", help="iTop server hostname", required=True)
-    parser.add_argument("-iu", metavar="ITOP_USERNAME", help="iTop server username", required=True)
-    parser.add_argument("-ip", metavar="ITOP_PASSWORD", help="iTop server password", required=True)
+    description = "Synchronize iTop CMDB CIs to LogicMonitor"
+    parser = argparse.ArgumentParser(description)
+    parser.add_argument("-c", "--company", metavar="CUSTOMER_PORTAL",
+                        help="LogicMonitor account", required=True)
+    parser.add_argument("-u", "--user", metavar="USERNAME",
+                        help="LogicMonitor username", required=True)
+    parser.add_argument("-p", "--password", metavar="PASSWORD",
+                        help="LogicMonitor password", required=True)
+    parser.add_argument("-ih", metavar="ITOP_HOST",
+                        help="iTop server hostname", required=True)
+    parser.add_argument("-iu", metavar="ITOP_USERNAME",
+                        help="iTop server username", required=True)
+    parser.add_argument("-ip", metavar="ITOP_PASSWORD",
+                        help="iTop server password", required=True)
     args = parser.parse_args()
-    
+
     # Define variables
     lm_agentid = "Collector_Description"    # LM Collector ID to add Devices to
-    
+
     # Define iTop parameters
     itop_params = {}
-    itop_params["host"]  = args.ih    # iTop server hostname
+    itop_params["host"] = args.ih    # iTop server hostname
     itop_params["user"] = args.iu    # iTop account
     itop_params["password"] = args.ip    # iTop password
-    itop_params["query_phrasebook"] = '1'     # iTop query phrasebook # for getting XML
+    itop_params["query_phrasebook"] = "1"     # iTop query phrasebook #
 
     # Define LogicMonitor parameters
     params = {}
@@ -77,27 +91,27 @@ def main():
     params["user"] = args.user
     params["password"] = args.password
 
-    
     group = Hostgroup(params)
     group.add()
 
-    # As of iTop 2.2, there is no "managementip" property for Printer or Hypervisor CIs, so only Server is synchronized in this script.
-    tree = getTree(itop_params, 'server')
+    # As of iTop 2.2, there is no "managementip" property for Printer or
+    # Hypervisor CIs, so only Server is synchronized in this script.
+    tree = getTree(itop_params, "server")
     print tree
-    
+
     # Iterate through all Servers
-    for elem in tree.iter('Server'):
+    for elem in tree.iter("Server"):
         # Get CI name and management IP
-        itop_name = str(elem.find('name').text)
-        itop_managementip = str(elem.find('managementip').text)
-        # Add to LM if there's an IP address / FQDN
-        if itop_managementip != 'None': 
+        itop_name = str(elem.find("name").text)
+        itop_managementip = str(elem.find("managementip").text)
+        # Add to LM if there"s an IP address / FQDN
+        if itop_managementip != "None":
             # Define Server Host parameters
             params["groups"] = ["iTop"]
             params["displayname"] = itop_name
             params["hostname"] = itop_managementip
             params["properties"] = {"itop.source": "yes"}
-            
+
             host = Host(params)
             host.add()
 
