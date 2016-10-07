fd6b20f8e1382d2fab4c09c3cf9fd5a5d6e2cd5a
Jeff Wozniak <jwozniak@SB-JWOZNIAK.local>
2016-10-07 14:38:46 -0700

    Fix line wrapping
    Ugh. My text editor wrap was set to 64 chars instead of 80. Undo the
    ugliness

--- logicmonitor_core/Collector.py
+++ logicmonitor_core/Collector.py
@@ -53,8 +53,8 @@ class Collector(LogicMonitor):
             self.path = self.resource + str(self.id)
 
     def create(self):
-        '''Idempotent function to make sure that there is
-        a running collector installed and registered'''
+        '''Idempotent function to make sure that there is a running collector
+        installed and registered'''
         logging.debug('Running Collector.create...')
 
         self._create()
@@ -64,8 +64,8 @@ class Collector(LogicMonitor):
         logging.debug('Collector created')
 
     def remove(self):
-        '''Idempotent function to make sure that there is
-        not a running collector installed and registered'''
+        '''Idempotent function to make sure that there is not a running
+        collector installed and registered'''
         logging.debug('Running Collector.destroy...')
 
         self.stop()
@@ -74,10 +74,8 @@ class Collector(LogicMonitor):
         logging.debug('Collector removed')
 
     def get_installer_binary(self):
-        '''Download the LogicMonitor collector
-        installer binary'''
-        logging.debug('Running ' +
-                      'Collector.get_installer_binary...')
+        '''Download the LogicMonitor collector installer binary'''
+        logging.debug('Running Collector.get_installer_binary...')
 
         self._os_check()
 
@@ -89,17 +87,15 @@ class Collector(LogicMonitor):
             logging.debug('32 bit system')
 
         if self.id is None:
-            self.fail(msg='Error: Unable  to retrieve the ' +
-                          'installer from the server')
+            self.fail(msg='Error: Unable  to retrieve the installer from ' +
+                          'the server')
 
         logging.debug('Agent ID is ' + str(self.id))
 
-        installfilepath = (self.installdir +
-                           '/logicmonitorsetup' + str(self.id) +
-                           '_' + str(arch) + '.bin')
+        installfilepath = (self.installdir + '/logicmonitorsetup' +
+                           str(self.id) + '_' + str(arch) + '.bin')
 
-        logging.debug('Looking for existing installer at ' +
-                      installfilepath)
+        logging.debug('Looking for existing installer at ' + installfilepath)
 
         if os.path.isfile(installfilepath):
             logging.debug('Collector installer already exists')
@@ -113,11 +109,9 @@ class Collector(LogicMonitor):
             # create install dir if it doesn't exist
             if not os.path.exists(self.installdir):
                 os.makedirs(self.installdir)
-                logging.debug('Created installdir at ' +
-                              self.installdir)
+                logging.debug('Created installdir at ' + self.installdir)
 
-            download_path = ('/setting/collectors/' +
-                             str(self.id) +
+            download_path = ('/setting/collectors/' + str(self.id) +
                              '/installers/linux' + str(arch))
             installer = self.api(download_path, 'GET')
             logging.debug('Writing installer file')
@@ -127,12 +121,11 @@ class Collector(LogicMonitor):
                             f.write(chunk)
             return installfilepath
         except Exception as err:
-            self.fail(msg='Unable to open installer file ' +
-                          'for writing. ' + str(err))
+            self.fail(msg='Unable to open installer file for writing. ' +
+                          str(err))
 
     def install(self):
-        '''Execute the LogicMonitor installer if not
-        already installed'''
+        '''Execute the LogicMonitor installer if not already installed'''
         logging.debug('Running Collector.install...')
 
         self._os_check()
@@ -158,19 +151,17 @@ class Collector(LogicMonitor):
             ret, err = p.communicate()
             cmd_result = p.returncode
             if cmd_result != 0:
-                self.fail(msg='Error: Unable to install ' +
-                              'collector: ' + str(ret))
+                self.fail(msg='Error: Unable to install collector: ' +
+                              str(ret))
             logging.debug('Collector installed successfully')
         except Exception as e:
-            self.fail('Error: Unable to installer collector. ' +
-                      str(e))
+            self.fail('Error: Unable to installer collector. ' + str(e))
 
     def uninstall(self):
         '''Uninstall LogicMontitor collector from the system'''
         logging.debug('Running Collector.uninstall...')
 
-        uninstallfile = (self.installdir +
-                         '/agent/bin/uninstall.pl')
+        uninstallfile = (self.installdir + '/agent/bin/uninstall.pl')
 
         if os.path.isfile(uninstallfile):
             logging.debug('Collector uninstall file exists')
@@ -184,20 +175,16 @@ class Collector(LogicMonitor):
                 ret, err = p.communicate()
                 cmd_result = p.returncode
                 if cmd_result != 0:
-                    self.fail(msg='Error: Unable to ' +
-                              'uninstall collector: ' +
+                    self.fail(msg='Error: Unable to uninstall collector: ' +
                               str(err))
 
                 logging.debug('Collector successfully uninstalled')
             except Exception as e:
-                self.fail('Error: Unable to uninstaller ' +
-                          'collector. ' + str(e))
+                self.fail('Error: Unable to uninstaller collector. ' + str(e))
         else:
             if os.path.exists(self.installdir + '/agent'):
-                self.fail(msg='Unable to uninstall ' +
-                              'LogicMonitor Collector. ' +
-                              'Can not find LogicMonitor ' +
-                              'uninstaller.')
+                self.fail(msg='Unable to uninstall LogicMonitor Collector. ' +
+                              'Can not find LogicMonitor uninstaller.')
 
     def start(self):
         '''Start the LogicMonitor collector'''
@@ -319,8 +306,7 @@ class Collector(LogicMonitor):
             self.fail(msg=resp["errmsg"])
 
     def site_facts(self):
-        '''Output current properties information
-        for the Collector'''
+        '''Output current properties information for the Collector'''
         logging.debug('Running Collector.site_facts...')
 
         if not self.info:
@@ -348,21 +334,18 @@ class Collector(LogicMonitor):
                 collector['description'] == self.description and
                 collector['description'] != ''
             ):
-                logging.debug('Collector matching ' +
-                              'description ' +
+                logging.debug('Collector matching description ' +
                               self.description + ' found.')
                 ret = collector
                 break
             elif str(collector['id']) == str(self.collector_id):
-                logging.debug('Collector id ' +
-                              self.collector_id + ' found.')
+                logging.debug('Collector id ' + self.collector_id + ' found.')
                 ret = collector
                 break
         return ret
 
     def _create(self):
-        '''Create a new collector in the associated
-        LogicMonitor account'''
+        '''Create a new collector in the associated LogicMonitor account'''
         logging.debug('Running Collector._create...')
 
         self._os_check()
@@ -389,8 +372,7 @@ class Collector(LogicMonitor):
             return resp['data']
 
     def get_collectors(self):
-        '''Returns a JSON object containing a list of
-        LogicMonitor collectors'''
+        '''Returns a JSON object containing a list of LogicMonitor collectors'''
         logging.debug('Running LogicMonitor.get_collectors...')
 
         resp = self.api(self.resource, 'GET')
@@ -402,8 +384,7 @@ class Collector(LogicMonitor):
             self.fail(msg='Failed to retrieve collectors')
 
     def _unregister(self):
-        '''Delete this collector from the associated
-        LogicMonitor account'''
+        '''Delete this collector from the associated LogicMonitor account'''
         logging.debug('Running Collector._unregister...')
 
         if self.info is None:
@@ -425,8 +406,7 @@ class Collector(LogicMonitor):
         else:
             # The collector couldn't unregister. Restart service
             logging.debug('Error unregistering collector.')
-            logging.debug('The collector service will be ' +
-                          'restarted')
+            logging.debug('The collector service will be restarted')
 
             self.start()
             self.fail(msg='Error unregistering collector.')
@@ -435,9 +415,8 @@ class Collector(LogicMonitor):
         if self.platform == 'Linux':
             logging.debug('Platform is Linux')
         else:
-            self.fail(msg='Error: LogicMonitor Collector ' +
-                          'must be installed on a Linux ' +
-                          'device.')
+            self.fail(msg='Error: LogicMonitor Collector must be installed ' +
+                          'on a Linux device.')
 
     def _changed(self, changed):
         self.change = changed
