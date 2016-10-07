811fd34bf24274dfd77cbf73f402980cab544b3e
Jeff Wozniak <jwozniak@SB-JWOZNIAK.local>
2016-10-06 17:14:47 -0700

    Don't fail on message body for DELETE

--- logicmonitor_core/LogicMonitor.py
+++ logicmonitor_core/LogicMonitor.py
@@ -97,8 +97,7 @@ class LogicMonitor(object):
         '''Make a call to the LogicMonitor server REST API'''
         logging.debug("Running LogicMonitor.rest...")
 
-        if ((method == 'DELETE' or
-             method == 'PATCH' or
+        if ((method == 'PATCH' or
              method == 'POST') and
            data is None):
             self.fail('Message body required for method ' + method)
