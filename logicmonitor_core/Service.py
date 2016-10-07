a29ec95b622e57495dd344c38e6c0c6ec4b9156c
Jeff Wozniak <jwozniak@SB-JWOZNIAK.local>
2016-10-07 11:11:07 -0700

    Improve usability of Service Class

--- logicmonitor_core/Service.py
+++ logicmonitor_core/Service.py
@@ -8,74 +8,100 @@ from subprocess import Popen
 
 class Service(object):
     @staticmethod
+    def start(name):
+        Service.doAction(name, 'start')
+
+    @staticmethod
+    def stop(name):
+        Service.doAction(name, 'stop')
+
+    @staticmethod
+    def restart(name):
+        Service.doAction(name, 'restart')
+
+    @staticmethod
     def getStatus(name):
         logging.basicConfig(level=logging.DEBUG)
-        logging.debug("Retrieving status of service " + name)
+        logging.debug('Retrieving status of service ' + name)
 
         ret = -1
 
         startType = Service._getType(name)
 
         # Determine how to get the status of the service
-        if startType == "init.d":
-            p = (Popen(["/etc/init.d/" + name, "status"],
+        if startType == 'init.d':
+            p = (Popen(['/etc/init.d/' + name, 'status'],
                        stdout=subprocess.PIPE))
             ret, err = p.communicate()
-        elif startType == "service":
-            p = (Popen(["service", name, "status"],
+        elif startType == 'service':
+            p = (Popen(['service', name, 'status'],
                        stdout=subprocess.PIPE))
             ret, err = p.communicate()
         else:
-            ret = "Unknown error getting status for service " + name
+            ret = ('Unknown error getting status for service ' +
+                   name)
 
+        if 'is running' in ret:
+            logging.debug('Service ' + name + ' is running')
+        else:
+            logging.debug('Service ' + name + ' is not running')
         return ret
 
     @staticmethod
     def doAction(name, action):
-        logging.debug("Performing " + action + " on service " + name)
+        logging.debug('Performing ' + action + ' on service ' + name)
 
         startType = Service._getType(name)
 
         ret = -1
 
         # Determine how to perform the action on the service
-        if startType == "init.d":
-            p = (Popen(["/etc/init.d/" + name, action],
+        if startType == 'init.d':
+            p = (Popen(['/etc/init.d/' + name, action],
                        stdout=subprocess.PIPE))
             msg = p.communicate()
             ret = p.returncode
-        elif startType == "service":
-            p = (Popen(["service", name, action],
+        elif startType == 'service':
+            p = (Popen(['service', name, action],
                        stdout=subprocess.PIPE))
             msg = p.communicate()
             ret = p.returncode
 
         else:
-            msg = ("Unknown error performing '" + action +
-                   "' on service " + name + "")
+            msg = ('Unknown error performing "' + action +
+                   '" on service ' + name)
             ret = msg
+
+        if ret == 0:
+            logging.debug(msg='successfully performed ' +
+                          action + ' on service ' +
+                          name + '. ')
+        else:
+            logging.error(msg='Error: Failed performing ' +
+                          action + ' on service ' +
+                          name + '. ')
 
         return (ret, msg)
 
     @staticmethod
     def _getType(name):
-        logging.debug("Getting service " + name + " control type")
+        logging.debug('Getting service ' + name + ' control type')
 
-        if os.path.isdir("/etc/init.d"):
-            logging.debug("Service control is via init.d")
-            return "init.d"
+        if os.path.isdir('/etc/init.d'):
+            logging.debug('Service control is via init.d')
+            return 'init.d'
         else:
             result = -1
 
         try:
-            p = (Popen(["service", "--status-all"],
+            p = (Popen(['service', '--status-all'],
                        stdout=subprocess.PIPE))
             p.communicate()
             result = p.returncode
 
             if result == 0 or result == 1:
-                logging.debug("Service control is via service")
-                return "service"
+                logging.debug('Service control is via service')
+                return 'service'
         except:
             result = -1
         return result
